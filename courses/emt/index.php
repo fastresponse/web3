2431e149c78aff68925a890e05cfb9096d305112
Brian Bridge <bbridge@fastresponse.org>
2016-07-28 17:26:13 -0700

    Footer is now 1 line. Added Social Media icons to footer. Edited colors of various menu and submenu items. Added example layout for EMT page. Moved main menu inside of the primary <section> tag. Course images +white borders. Decreased main menu line-height. Made center column scroll in postsec pages. TODO: Make images on postsec pages keep their size ratios when resizing page.

--- courses/emt/index.php
+++ courses/emt/index.php
@@ -4,12 +4,33 @@ require_once '../../twig_loader.php';
 
 $course_code = 'EMT';
 $form_course_code = 'EMT';
+$course_color = '#144995';
 
 $content = <<<'HTML'
+<h2>Five Reasons Why Students Are Enrolling In The EMT Program At Fast Response</h2>
+<ol>
+  <li>NREMT pass rate is higher than the national pass rate</li>
+  <li>Externships with 7 major ambulance companies</li>
+  <li>Our students are well-known by our externship sites, which provides a direct pipeline to jobs</li>
+  <li>90% of our students find jobs!</li>
+  <li>Real world experience - most of our instructors are active EMTs</li>
+</ol>
+
+<p id="testimonial-link"><a href="/testimonials/">Read what our students have to say</a></p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisi ante, vulputate eu rutrum non, euismod sed lectus. Ut ac mi augue, semper molestie lectus. Nam a nulla vitae justo porta lobortis. Etiam cursus dictum auctor. Nam eu lacus ante. Ut at nibh et leo volutpat adipiscing at at diam. Sed sit amet congue enim. Fusce purus mi, accumsan ac ultrices eu, mollis in risus. Fusce ac lectus luctus lorem tincidunt tristique. Duis interdum fermentum vehicula. Phasellus dui tortor, tristique rutrum consectetur vel, molestie ut quam. Suspendisse non enim sit amet lorem eleifend facilisis.</p>
 HTML;
 
-$left_images = array();
-$right_images = array();
+$left_images = array(
+  '/img/emt/slide02.jpg',
+  '/img/emt/EMT Skills 03-2013 (136).png',
+  '/img/emt/slide03.jpg',
+);
+$right_images = array(
+  '/img/emt/emt skills lab 005.jpg',
+  '/img/emt/slide04.jpg',
+  '/img/emt/slide05.jpg',
+);
 
 
 $renderer = new TemplateRenderer();
@@ -22,6 +43,7 @@ print $renderer->render('postsec.twig', array(
   'canonical' => 'http://www.fastresponse.org/courses/emt/',
   'course_code' => $course_code,
   'form_course_code' => $form_course_code,
+  'course_color' => $course_color,
   'content' => $content,
   'left_images' => $left_images,
   'right_images' => $right_images,
