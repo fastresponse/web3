906583c02adefe6291e4e1e06041e23614ea15d4
Brian Bridge <bbridge@fastresponse.org>
2016-08-02 17:16:39 -0700

    Moved some styles to the main.css file with overrides for index. Added About page. Edited templates so displaying page_header (an h1 tag) is more intuitive.

---
+++ about/index.php
@@ -0,0 +1,29 @@
+<?php
+require_once '../twig_loader.php';
+
+$content = <<<'HTML'
+<h2>We are approved by the <a href="http://www.bppe.ca.gov/about_us/contact.shtml">Bureau of Private and Postsecondary Education</a></h2>
+<p class="bold italic">Fast Response School of Health Care Education is a private postsecondary educational school.</p>
+<p>We are an American Heart Association (AHA), American Safety and Health Institute (ASHI) and American Academy of Pediatrics (AAP) affiliated training facility. We provide CEUs (Continuing Education Units) for EMS personnel, Nurses and other medical professionals.</p>
+<p>Our Phlebotomy Technician program is approved by the State of California Department of Health Services, and our Emergency Medical Technician program is approved by the State of California Emergency Medical Services Authority.</p>
+<p>As a School of Health Care Education, our curriculum is approved by The Department of Health Services California Laboratory Field Services, California Emergency Medical Services Authority (EMSA), and registered with The Bureau of Private Postsecondary Vocational Education. To maintain this status, we must uphold the same standards as a Junior College, University or Vocational School, but we can offer comparable training in a shorter period of time, providing you with superior training while you begin or augment your career in the medical field.</p>
+<p>In addition to providing training at our main campus in Berkeley, we also teach in a variety of healthcare and corporate settings in the greater Bay Area. We contract with hospitals, businesses, schools, daycares, community organizations, and individuals.</p>
+<p>Since 1989, Fast Response School of Health Care Education has been the Bay Area's premier community and professional training center, offering certifications in many postsecondary and continuing education courses.</p>
+<p>We promote an energetic, diverse, and challenging atmosphere where you are given the attention needed to lay the groundwork for success in today's exciting healthcare industry.  Our Instructors are all certified or licensed medical professionals with years of field and teaching experience. Our school administrators are invested in the success of every student.</p>
+<p>Fast Response is a fifty percent woman-owned (and family-owned), equal opportunity business, with women and minorities making up a large percentage of our staff.</p>
+<hr>
+<h2>Our Mission</h2>
+<p>To create a highly stimulating and supportive learning environment which will enable our students to obtain the necessary career skills to achieve their life goals. We sincerely believe that a learning environment which is challenging, accepting and lively inspires confidence and motivates our students to succeed. Together, we can be a driving factor in improving the quality of Health Care in our community.</p>
+HTML;
+
+$renderer = new TemplateRenderer();
+
+print $renderer->render('index.twig', array(
+  'title' => 'Fast Response School of Health Care Education', 
+  'page_header' => 'About Fast Response',
+  'description' => "Fast Response offers the Bay Area's most comprehensive education and resources for healthcare professionals including entry-level courses, certifications, and continuing education units.",
+  'keywords' => 'fast response, fast, response, health care, healthcare, school, education, training, certification, emt, emergency medical technician, medical assistant, sterile processing, phlebotomy, phlebotomist, pharmacy tech, pharmacy technician, paramedic, continuing education, cpr, bls, basic life support, acls, advanced life support, ecg, ekg',
+  'canonical' => 'http://www.fastresponse.org/',
+  'content' => $content,
+));
+?>
