70e1fba1956c0ce9295c749d4b593b3ad4109c08
Brian Bridge <bbridge@fastresponse.org>
2016-08-04 13:28:03 -0700

    Styled scrollbars, set min width on <main>, added some page-specific styles for testimonials and contact pages, created contact us page with mockup information, changed testimonials to use <ul> instead of <dl>, spaced testimonials out vertically.

--- testimonials/index.php
+++ testimonials/index.php
@@ -2,16 +2,22 @@
 require_once '../twig_loader.php';
 
 $content = <<<'HTML'
-<dl>
-  <dt>Firstname Lastname - Graduate, 2015</dt>
-  <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisi ante, vulputate eu rutrum non, euismod sed lectus. Ut ac mi augue, semper molestie lectus.</dd>
+<ul class="testimonials">
+  <li>
+    <div>Firstname Lastname - Graduate, 2015</div>
+    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nisi ante, vulputate eu rutrum non, euismod sed lectus. Ut ac mi augue, semper molestie lectus.</div>
+  </li>
 
-  <dt>Person McHuman - Director, Crazy Ambulance Co</dt>
-  <dd>Nam a nulla vitae justo porta lobortis. Etiam cursus dictum auctor. Nam eu lacus ante. Ut at nibh et leo volutpat adipiscing at at diam. Sed sit amet congue enim. Fusce purus mi, accumsan ac ultrices eu, mollis in risus.</dd>
+  <li>
+    <div>Person McHuman - Director, Crazy Ambulance Co</div>
+    <div>Nam a nulla vitae justo porta lobortis. Etiam cursus dictum auctor. Nam eu lacus ante. Ut at nibh et leo volutpat adipiscing at at diam. Sed sit amet congue enim. Fusce purus mi, accumsan ac ultrices eu, mollis in risus.</div>
+  </li>
 
-  <dt>Robot Testdata - Graduate, 2014</dt>
-  <dd>Fusce ac lectus luctus lorem tincidunt tristique. Duis interdum fermentum vehicula. Phasellus dui tortor, tristique rutrum consectetur vel, molestie ut quam. Suspendisse non enim sit amet lorem eleifend facilisis.</dd>
-</dl>
+  <li>
+    <div>Robot Testdata - Graduate, 2014</div>
+    <div>Fusce ac lectus luctus lorem tincidunt tristique. Duis interdum fermentum vehicula. Phasellus dui tortor, tristique rutrum consectetur vel, molestie ut quam. Suspendisse non enim sit amet lorem eleifend facilisis.</div>
+  </li>
+</ul>
 HTML;
 
 $left_images = array(
@@ -34,6 +40,7 @@ print $renderer->render('postsec.twig', array(
   'description' => '',
   'keywords' => '',
   'canonical' => 'http://www.fastresponse.org/testimonials/',
+  'css' => array('testimonials.css'),
   'content' => $content,
   'left_images' => $left_images,
   'right_images' => $right_images,
